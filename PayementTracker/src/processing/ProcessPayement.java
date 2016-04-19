fc7cde2a1a9973c65a7d4c6a1ccd18a8770f1f6b
valiclud <valicek@atlas.cz>
2016-04-19 18:33:26 +0200

    Adding junit tests for ProcessPayement class

--- PayementTracker/src/processing/ProcessPayement.java
+++ PayementTracker/src/processing/ProcessPayement.java
@@ -1,9 +1,7 @@
 
 package processing;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
 
@@ -25,6 +23,7 @@ public class ProcessPayement {
 	 * Instantiates a new process payement.
 	 */
 	private ProcessPayement() {
+		throw new UnsupportedOperationException();
 	}
 
 	/**
